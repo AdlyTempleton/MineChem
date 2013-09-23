96877264723ee857a7d673d7911eddaf225e832f
Mandrake Naader Fernflower <machacker9000@gmail.com>
2013-09-23 19:29:01 -0400

    Fix array underflow

--- src/ljdp/minechem/common/items/ItemBlockFusion.java
+++ src/ljdp/minechem/common/items/ItemBlockFusion.java
@@ -5,7 +5,7 @@ import net.minecraft.item.ItemStack;
 
 public class ItemBlockFusion extends ItemBlock {
 
-    private static final String[] names = { "Fusion Wall", "Tungsten Plating", "Fusion Core" };
+    private static final String[] names = { "Fusion Wall", "Tungsten Plating", "Fusion Core", "Error" }; // Java indexes start at zero, NOT ONE! 
 
     public ItemBlockFusion(int par1) {
         super(par1);
